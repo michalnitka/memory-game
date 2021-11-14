3b837747269c324ca599925df2749f06432cf6a2
Michal <grandpress91@gmail.com>
2021-11-14 17:46:09 +0100

    Init project + first steps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const cardImages = [
+  { src: "/public/img/Chase_PNG.png" },
+  { src: "/public/img/Everest_sitinig.png" },
+  { src: "/public/img/Marshall.png" },
+  { src: "/public/img/Rubble.png" },
+  { src: "/public/img/Skye.png" },
+  { src: "/public/img/Zuma.png" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory game</h1>
+      <button>Nowa gra</button>
     </div>
   );
 }
