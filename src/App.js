18c2e0581bf9b763fdfc87ef99408636dc3c6659
Michał <michal.nitka@bauer.pl>
2021-11-23 13:04:00 +0100

    Disabled multiple click

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -32,6 +33,7 @@ function App() {
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards((prevState) => {
           return prevState.map((card) => {
@@ -44,8 +46,7 @@ function App() {
         });
         resetTurn();
       } else {
-        setTimeout(()=>resetTurn(),1000)
-
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
@@ -54,6 +55,7 @@ function App() {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prevState) => prevState + 1);
+    setDisabled(false);
   };
 
   return (
@@ -63,7 +65,13 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <Card key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched}/>
+          <Card
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import "./Card.css";
 
-export default function Card({ card, handleChoice, flipped }) {
+export default function Card({ card, handleChoice, flipped, disabled }) {
   const handleClick = () => {
-    handleChoice(card);
+    if (!disabled) {
+      handleChoice(card);
+    }
   };
 
   return (
     <div className="card">
-      <div className={flipped? "flipped" : ''}>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" />
         <img
           className="cover"
